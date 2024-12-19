8a10995d0ff6b34ce97c0e1c82f048a3111716f4
DawnGameDev <76510327+DawnGameDev@users.noreply.github.com>
2024-12-19 01:08:48 -0500

    Initial commit

---
+++ src/main/java/net/dawndev/chiselandbitsarmory/modid/ChiselandBitsArmoryClient.java
@@ -0,0 +1,10 @@
+package net.dawndev.chiselandbitsarmory.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChiselandBitsArmoryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
